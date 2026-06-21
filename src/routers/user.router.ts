26aec76809c3acceacdcdada52602eee16130a23
Sayan Dutta <168883129+NinjaOP241@users.noreply.github.com>
2026-06-22 00:09:00 +0530

    Added api for get user by id

--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { findAllUsers } from "../controllers/user.controller.js";
+import { findAllUsers, findById } from "../controllers/user.controller.js";
 
 // We will see the routes after /users
 export const userRouter: Router = Router();
 
 userRouter.get("/", findAllUsers);
+userRouter.get("/:id", findById);
