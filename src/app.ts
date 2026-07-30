9f8c607df98d6730967b5459802f369599744131
Sayan Dutta <168883129+NinjaOP241@users.noreply.github.com>
2026-07-30 14:27:25 +0530

    Mounted availability router at /api/availability

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import express, { Express } from "express";
 import { userRouter } from "./routers/user.router.js";
 import { publicEventRouter } from "./routers/public-event.router.js";
 import { eventTypeRouter } from "./routers/event-type.router.js";
+import { availabilityRouter } from "./routers/availability.router.js";
 
 import { errorHandler } from "./middlewares/error-handler.js";
 import { routeNotFound } from "./middlewares/route-not-found.js";
@@ -24,6 +25,7 @@ app.get("/health", (_req, res) => {
 app.use("/api/users", userRouter);
 app.use("/api/event-types", eventTypeRouter);
 app.use("/api/public", publicEventRouter);
+app.use("api/availability", availabilityRouter);
 
 /**
  * The order of middlewares is important in Express.
