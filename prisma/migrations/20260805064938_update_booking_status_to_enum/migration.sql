0cbad389549ebf4f3de0ff4062b58757fdfb4b40
Sayan Dutta <168883129+NinjaOP241@users.noreply.github.com>
2026-08-05 12:20:17 +0530

    updated booking status to enum

---
+++ prisma/migrations/20260805064938_update_booking_status_to_enum/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - The `status` column on the `bookings` table would be dropped and recreated. This will lead to data loss if there is data in the column.
+
+*/
+-- AlterTable
+ALTER TABLE "bookings" DROP COLUMN "status",
+ADD COLUMN     "status" "BookingStatus" NOT NULL DEFAULT 'PENDING';
+
+-- CreateIndex
+CREATE INDEX "bookings_status_idx" ON "bookings"("status");
